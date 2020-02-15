d3ef8bfce8733e1d6db5314c69597be147e392bf
Christof Sirk <sirchb01@gmail.com>
2020-02-15 22:32:02 +0100

    Add some more git commands and use an approved verb

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,11 +2,27 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 
-function git-status {
+function Invoke-Git-Status {
     & git 'status'
 }
 
-New-Alias g git-status
+function Invoke-Git-Fetch {
+    & git 'fetch'
+}
+
+function Invoke-Git-Pull {
+    & git 'fetch'
+}
+
+function Invoke-Git-Push {
+    & git 'push'
+}
+
+New-Alias g Invoke-Git-Status
+New-Alias gf Invoke-Git-Fetch
+New-Alias gl Invoke-Git-Pull
+New-Alias gp Invoke-Git-Push
+
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
