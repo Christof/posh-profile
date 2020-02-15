5a38671ff8e5f618575f49b48656f2bdad705329
Christof Sirk <sirchb01@gmail.com>
2020-02-15 22:39:08 +0100

    Add ga alias and remove already defined aliases for gl and gp

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,12 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 
+function Remove-Alias ([string] $AliasName) {
+	while (Test-Path Alias:$AliasName) {
+		Remove-Item Alias:$AliasName -Force 2> $null
+	}
+}
+
 function Invoke-Git-Status {
     & git 'status'
 }
@@ -18,10 +24,18 @@ function Invoke-Git-Push {
     & git 'push'
 }
 
+function Invoke-Git-Add {
+    & git 'add' $args
+}
+
+Remove-Alias gl
+Remove-Alias gp
+
 New-Alias g Invoke-Git-Status
 New-Alias gf Invoke-Git-Fetch
 New-Alias gl Invoke-Git-Pull
 New-Alias gp Invoke-Git-Push
+New-Alias ga Invoke-Git-Add
 
 
 # Chocolatey profile
